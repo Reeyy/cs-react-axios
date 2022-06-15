1c43cc2d0a515c428459bf8015ab24ab467f2ff5
Reyhan Muhammad Januar <reyhanmuhammadj@gmail.com>
2022-06-15 18:46:41 +0700

    fix finish

--- src/Pages/Navbar.jsx
+++ src/Pages/Navbar.jsx
@@ -4,7 +4,9 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <div className="w-full h-16 bg-gradient-to-r from-orange-400 to-pink-600 flex items-center px-10 justify-between">
-      <h1 className="text-3xl font-semibold font-Firacode text-white">Crud</h1>
+      <Link to="/" className="text-3xl font-semibold font-Firacode text-white">
+        Crud
+      </Link>
       <Link
         to="/add-user"
         className="relative inline-block group focus:outline-none focus:ring "
